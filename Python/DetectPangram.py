082a1466e59fd2233b10329f1c15ffc86427e600
OscarCanongo <oscarcanongo@outlook.com>
2021-02-13 14:21:21 -0600

    Detect Pangram

---
+++ Python/DetectPangram.py
@@ -0,0 +1,8 @@
+import string
+
+def is_pangram(s):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for x in alphabet:
+        if(not x in s.lower()):
+            return False
+    return True
